d5ab25abd2edf894d95ca905768cfa9bcf1fa7f9
Casey Schneider-Mizell <caseysm@gmail.com>
2026-04-16 18:53:36 -0400

    v1.2.0

--- src/datafolio/__init__.py
+++ src/datafolio/__init__.py
@@ -1,6 +1,6 @@
 """datafolio: Lightweight wrapping of dataframes, models, and metadata to track analyses."""
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 from datafolio.folio import DataFolio
 
